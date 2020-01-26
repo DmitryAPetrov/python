8c992577fc78c69cb0754d8de7a71de0b76555fe
Ditry Petrov <P.Dm.An@ya.ru>
2020-01-26 20:49:11 +0300

    dz1

---
+++ DZ1.py
@@ -0,0 +1,49 @@
+##----------------------Задание №1----------------------
+# nameUser = input("Введите свое имя:   ")
+# surnameUser =input("Введите свою фамилию:  ")
+# print(f"Добрый день {nameUser} {surnameUser}")
+
+##----------------------Задание №2----------------------
+# second = int(input("Введите длительность времени в секундах:  "))
+# minute = second//60
+# second = second - minute*60
+#
+# clock = minute//60
+#
+# minute = minute - clock*60
+#
+# print(f'Конвертация (чч:мм:сс) -  {clock}:{minute}:{second}')
+
+# #----------------------Задание №3----------------------
+#
+# numberUser = input("Введите число:   ")
+#
+# n = numberUser
+# nn = n + numberUser
+# nnn = nn + numberUser
+#
+# result = int(n) + int(nn) + int(nnn)
+# print(f"Cумму чисел n + nn + nnn = {result}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
